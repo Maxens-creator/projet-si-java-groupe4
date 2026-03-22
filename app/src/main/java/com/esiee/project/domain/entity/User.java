dcba4fed8a8fcd85a78d279c70d992c4e6e4456e
Maxens-creator <maxens.constant2006@gmail.com>
2026-03-22 15:07:36 +0100

    feat(domain): add Comment entity

---
+++ app/src/main/java/com/esiee/project/domain/entity/User.java
@@ -0,0 +1,36 @@
+package com.esiee.project.domain.entity;
+
+import com.esiee.project.domain.enumtype.Role;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.ROLE_USER;
+
+    public User() {}
+
+    public User(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() { return id; }
+}
